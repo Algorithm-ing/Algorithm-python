9e4a32e1aaebda6aa579d626fcc8c3482be59571
SeryeongK <99seryoung@naver.com>
2024-11-30 12:23:40 +0900

    2주차 5문제 풀이 추가

---
+++ 수학/2628_종이자르기_Silver5/세령_32ms_31120KB.py
@@ -0,0 +1,31 @@
+a, b = map(int, input().split())
+x = [0, a]
+y = [0, b]
+n = int(input())
+input = [input() for _ in range(n)]
+
+# 가로 세로 선 추가
+for i in input:
+    a, b = map(int, i.split())
+    if a == 0:
+        y.append(b)
+    else:
+        x.append(b)
+
+x.sort(reverse=True)
+y.sort(reverse=True)
+
+# 가장 차이가 많이 나는 가로 세로 길이
+maxX = 0
+maxY = 0
+for i in range(len(x)-1):
+    gapX = x[i]-x[i+1]
+    if gapX > maxX:
+        maxX = gapX
+
+for i in range(len(y)-1):
+    gapY = y[i]-y[i+1]
+    if gapY > maxY:
+        maxY = gapY
+
+print(maxX * maxY)
---
+++ 재귀함수/10872_팩토리얼_Bronze3/세령_32ms_31120KB.py
@@ -0,0 +1,11 @@
+input = int(input())
+
+
+def factorial(n):
+    if (n > 0):
+        return n * factorial(n-1)
+    else:
+        return 1
+
+
+print(factorial(input))
